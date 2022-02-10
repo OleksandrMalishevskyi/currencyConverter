a734c9c0bf2dc00566eee877f7dad71d910821e9
Aleksandr <oleksandr.malishevskyi@gmail.com>
2022-02-10 12:55:12 +0100

    first-last

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = (props) => {
+  const [value, setValue] = useState(props.amountUAH);
+  let bankData;
+
+  useEffect(async () => {
+       bankData = await fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+             .then((response) => {
+               return response.json();
+              })
+             .then((data) => {           
+                  return data.map(item => ({
+                        currencyСode: item.r030,
+                        currencyValue: item.rate
+                    }));        
+      })
+  });
+
+ const currencyСonversion = (currencyСode) => {
+   reset();
+   for(let i = 0; i < bankData.length; i++){
+       if(bankData[i].currencyСode === currencyСode) {
+         setValue(value => (value/bankData[i].currencyValue).toFixed(2));      
+       }
+   }
+ }
+ 
+ function reset() {
+     setValue(value => value = props.amountUAH);
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="counter">{value}</div>
+        <div className="controls">
+          <button onClick={() => currencyСonversion(840)}>USD</button>
+          <button onClick={() => currencyСonversion(978)}>EUR</button>
+          <button onClick={() => currencyСonversion(643)}>RUB</button>
+          <button onClick={reset}>RESET</button>
+        </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
